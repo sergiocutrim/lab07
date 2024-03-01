156066ccb4991e9b1421b9c5c15f860d8dbf9e21
sergiocutrim <97315931+sergiocutrim@users.noreply.github.com>
2024-03-01 11:29:18 -0500

    Delete includes/footer-nav.php

--- includes/footer-nav.php
+++
@@ -1,3 +0,0 @@
-<nav>
-	<a href="/week-7-complete/about/privacy-policy.php" title="Read our Privacy Policy">Privacy Policy</a> | <a href="/week-7-complete/about/privacy-policy.php#personal-information" title="Learn about how we treat your personal information">Use of Personal Information</a> | <a href="/week-7-complete/about/terms-of-use.php" title="Understand our Terms of Use">Terms of Use</a> | <a href="/week-7-complete/about/terms-of-use.php#limitation-liability" title="Our Limitation of Liability explained">Limitation of Liability</a>
-</nav>
